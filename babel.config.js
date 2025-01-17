eb65a589d03b7b20884e91c0722e37ca474c8b3a
Durgesh Kumar Dwivedi <durgeshdwivedi81@gmail.com>
2025-01-17 19:41:39 +0530

    first test-release

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,41 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: ['babel-preset-expo'],
-      plugins: [
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['tamagui'],
+          config: './tamagui.config.ts',
+          logTimings: true,
+          disableExtraction: process.env.NODE_ENV === 'development',
+        },
+      ],
+      [
+        'module-resolver',
+        {
+          root: ['./src/app'],
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+          alias: {
+            '@constants': './src/constants',
+            '@components': './src/components',
+            '@hooks': './src/hooks',
+            '@hooks': './src/hooks',
+          },
+        },
+      ],
       'react-native-reanimated/plugin',
     ],
-    };
   };
-  
+};
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,13 +2,29 @@ import { ThemedView } from '@/components/ThemedView';
 import React from 'react';
 import MediaBrowser from '@/components/MediaBrowser';
 import { MediaType } from '@/constants/types';
+import { Text } from 'tamagui';
 
 const Anime = () => {
   return (
     <ThemedView>
-      <MediaBrowser mediaType={MediaType.ANIME} />
+      {/* <MediaBrowser mediaType={MediaType.ANIME} /> */}
+      <Text>{process.env.NODE_ENV}</Text>
+      <Text>{process.env.EXPO_PUBLIC_API_URL}</Text>
+      <Text>{process.env.EXPO_PUBLIC_API_URL_DEV}</Text>
+      <Text>{process.env.EXPO_PUBLIC_EPISODE_API_URL}</Text>
+      <Text>{process.env.EXPO_PUBLIC_EPISODE_API_URL_DEV}</Text>
     </ThemedView>
   );
 };
+
+// [
+//   'module-resolver',
+//   {
+//     root: ['./'],
+//     alias: {
+//       '@': './src',
+//     },
+//   },
+// ],
 
 export default Anime;
