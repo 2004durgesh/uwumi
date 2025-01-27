8e3eb5d9c482e14086a18814daddb80ebce20c90
Durgesh Kumar Dwivedi <durgeshdwivedi81@gmail.com>
2025-01-27 19:05:41 +0530

    git issue resolving

--- src/app/watch/ControlsOverlay.tsx
+++ src/app/watch/ControlsOverlay.tsx
@@ -1,49 +1,69 @@
-import React, { useState } from 'react';
-import { YStack, XStack, Button, Text, View, Slider } from 'tamagui';
-import { Play, Pause, Volume2, VolumeOff, Maximize, Minimize, Settings } from '@tamagui/lucide-icons';
+import React, { memo, useEffect, useState } from 'react';
+import { YStack, XStack, Button, Text, View, Slider, Sheet, Spinner, Tabs } from 'tamagui';
+import {
+  Play,
+  Pause,
+  Volume2,
+  VolumeOff,
+  Maximize,
+  Minimize,
+  Settings,
+  Captions,
+  CaptionsOff,
+  SkipForward,
+  SkipBack,
+} from '@tamagui/lucide-icons';
 import Animated, { FadeIn, FadeOut, Easing } from 'react-native-reanimated';
-import { Pressable } from 'react-native';
 import { ISubtitle } from '@/constants/types';
+import { useRouter } from 'expo-router';
+import { useEpisodesIdStore, useEpisodesStore } from '@/hooks';
+import { formatTime } from '@/constants/utils';
+import { VideoTrack } from './[mediaType]';
+import RippleButton from '@/components/RippleButton';
+
 
 interface ControlsOverlayProps {
   showControls: boolean;
+  routeInfo: {
+    mediaType: string;
+    provider: string;
+    id: string;
+  };
   isPlaying: boolean;
   isMuted: boolean;
   isFullscreen: boolean;
+  isBuffering: boolean;
   subtitleTracks: ISubtitle[] | undefined;
   selectedSubtitleIndex: number | undefined;
   setSelectedSubtitleIndex: (index: number | undefined) => void;
+  videoTracks: VideoTrack[] | undefined;
+  selectedVideoTrackIndex: number | undefined;
+  setSelectedVideoTrackIndex: (height: number | undefined) => void;
   currentTime: number;
   seekableDuration: number;
   title: string;
-  onPlayPress: (e: any) => void;
-  onMutePress: (e: any) => void;
-  onFullscreenPress: (e: any) => void;
+  onPlayPress: () => void;
+  onMutePress: () => void;
+  onFullscreenPress: () => void;
   onSeek: (time: number) => void;
 }
 
-export const formatTime = (seconds: number): string => {
-  const h = Math.floor(seconds / 3600);
-  const m = Math.floor((seconds % 3600) / 60);
-  const s = Math.floor(seconds % 60);
-
-  if (h > 0) {
-    return `${h}:${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}`;
-  }
-  return `${m}:${s.toString().padStart(2, '0')}`;
-};
-
 const AnimatedYStack = Animated.createAnimatedComponent(YStack);
 
-const ControlsOverlay = React.memo(
+const ControlsOverlay = memo(
   ({
     showControls,
+    routeInfo,
     isPlaying,
     isMuted,
     isFullscreen,
+    isBuffering,
     subtitleTracks,
     selectedSubtitleIndex,
     setSelectedSubtitleIndex,
+    videoTracks,
+    selectedVideoTrackIndex,
+    setSelectedVideoTrackIndex,
     currentTime,
     seekableDuration,
     title,
@@ -53,12 +73,32 @@ const ControlsOverlay = React.memo(
     onSeek,
   }: ControlsOverlayProps) => {
     const [openSettings, setOpenSettings] = useState(false);
+    // console.log("settings are being shown", openSettings);
+    const router = useRouter();
+    const { mediaType, provider, id } = routeInfo;
+    const prevEpisodeId = useEpisodesIdStore((state) => state.prevEpisodeId);
+    const currentEpisodeId = useEpisodesIdStore((state) => state.currentEpisodeId);
+    const nextEpisodeId = useEpisodesIdStore((state) => state.nextEpisodeId);
+    const setEpisodeIds = useEpisodesIdStore((state) => state.setEpisodeIds);
+    const episodes = useEpisodesStore((state) => state.episodes);
+    const currentEpisodeIndex = episodes.findIndex((ep) => ep.id === currentEpisodeId);
+    const prevEpisodeIndex = episodes.findIndex((ep) => ep.id === prevEpisodeId);
+    const nextEpisodeIndex = episodes.findIndex((ep) => ep.id === nextEpisodeId);
+    const prevId = currentEpisodeIndex > 0 ? episodes[currentEpisodeIndex - 1].id : null;
+    const nextId = currentEpisodeIndex < episodes.length - 1 ? episodes[currentEpisodeIndex + 1].id : null;
+    // temp
+    useEffect(() => {
+      if (prevId || nextId) {
+        setEpisodeIds(currentEpisodeId!, prevId, nextId);
+      }
+    }, [currentEpisodeId, prevId, nextId, setEpisodeIds]);
+
     return showControls ? (
       <>
         <AnimatedYStack
           flex={1}
           justifyContent="space-between"
-          padding="$2"
+          paddingVertical={isFullscreen ? '$5' : '$2'}
           backgroundColor="rgba(0, 0, 0, 0.5)"
           entering={FadeIn.duration(300).easing(Easing.bezierFn(0.25, 0.1, 0.25, 1))}
           exiting={FadeOut.duration(100).easing(Easing.out(Easing.ease))}>
@@ -66,21 +106,32 @@ const ControlsOverlay = React.memo(
             <Text color="white" fontWeight={700} fontSize="$3.5">
               {title}
             </Text>
-            <XStack gap="$2">
-              <Pressable onPress={() => setOpenSettings(!openSettings)}>
+            <XStack gap="$4">
+              {(selectedSubtitleIndex ?? -1) > -1 ? (
+                <RippleButton onPress={() => setSelectedSubtitleIndex(-1)}>
+                  <Captions color="white" size={20} />
+                </RippleButton>
+              ) : (
+                <RippleButton onPress={() => setSelectedSubtitleIndex(0)}>
+                  <CaptionsOff color="white" size={20} />
+                </RippleButton>
+              )}
+              <RippleButton
+                onPress={() => {
+                  setOpenSettings(!openSettings);
+                }}>
                 <Settings color="white" size={20} />
-              </Pressable>
-              {/* <Sheet
+              </RippleButton>
+              <Sheet
                 forceRemoveScrollEnabled={false}
                 modal={true}
                 open={openSettings}
                 onOpenChange={(value: boolean) => setOpenSettings(value)}
-                snapPoints={isFullscreen?[80,25]:[50, 25]}
+                snapPoints={isFullscreen ? [80, 25] : [50, 25]}
                 snapPointsMode={'percent'}
                 dismissOnSnapToBottom
                 zIndex={100_000}
-                animation="quick"
-              >
+                animation="quick">
                 <Sheet.Handle backgroundColor="$color4" />
                 <Sheet.Overlay
                   backgroundColor="transparent"
@@ -88,49 +139,144 @@ const ControlsOverlay = React.memo(
                   enterStyle={{ opacity: 0 }}
                   exitStyle={{ opacity: 0 }}
                 />
-                <Sheet.Frame backgroundColor="#0e0f15" marginHorizontal="auto" width="95%">
+                <Sheet.Frame backgroundColor="#0e0f15" marginHorizontal="auto" width={isFullscreen ? '50%' : '90%'}>
                   <Sheet.ScrollView>
-                    <YStack gap="$2">
-                      <Button
-                        backgroundColor={selectedSubtitleIndex === undefined ? '$color' : "#0e0f15"}
-                        onPress={() => {
-                          setSelectedSubtitleIndex(undefined);
-                        }}
-                      >
-                        Off
-                      </Button>
-                      {subtitleTracks?.map((track, index) => (
-                        <Button
-                          key={index}
-                          backgroundColor={"#0e0f15"}
-                          color={selectedSubtitleIndex === index ? '$color' : "$color1"}
-                          onPress={() => {
-                            setSelectedSubtitleIndex(index);
-                          }}
-                        >
-                          {track.lang}
-                        </Button>
-                      ))}
-                    </YStack>
+                    <Tabs
+                      defaultValue="tab1"
+                      orientation="horizontal"
+                      flexDirection="column"
+                      // width={'100%'}
+                      // height={150}
+                      borderRadius="$4"
+                      overflow="hidden"
+                      borderColor="$borderColor">
+                      <Tabs.List disablePassBorderRadius="bottom">
+                        <Tabs.Tab flex={1} value="tab1">
+                          <Text>Quality</Text>
+                        </Tabs.Tab>
+                        <Tabs.Tab flex={1} value="tab2">
+                          <Text>Subtitle</Text>
+                        </Tabs.Tab>
+                        <Tabs.Tab flex={1} value="tab3">
+                          <Text>Audio</Text>
+                        </Tabs.Tab>
+                      </Tabs.List>
+                      <Tabs.Content value="tab1">
+                        <YStack flex={1} width="100%" gap="$2" alignSelf="flex-start" paddingHorizontal="$4">
+                          {videoTracks?.map((track, index) => (
+                            <RippleButton
+                              key={index}
+                              style={{
+                                backgroundColor: '#0e0f15',
+                              }}
+                              onPress={() => {
+                                console.log(index, selectedSubtitleIndex, track.index);
+                                setSelectedVideoTrackIndex(track.index);
+                              }}>
+                              <Text color={selectedVideoTrackIndex === track.index ? '$color' : '$color1'}>
+                                {track.height}p
+                              </Text>
+                            </RippleButton>
+                          ))}
+                        </YStack>
+                      </Tabs.Content>
+
+                      <Tabs.Content value="tab2">
+                        <YStack flex={1} width="100%" gap="$2" alignSelf="flex-start" paddingHorizontal="$4">
+                          {subtitleTracks?.map((track, index) => (
+                            <RippleButton
+                              key={index}
+                              style={{
+                                backgroundColor: '#0e0f15',
+                              }}
+                              onPress={() => {
+                                setSelectedSubtitleIndex(index);
+                              }}>
+                              <Text color={selectedSubtitleIndex === index ? '$color' : '$color1'}>{track.lang}</Text>
+                            </RippleButton>
+                          ))}
+                        </YStack>
+                      </Tabs.Content>
+
+                      <Tabs.Content value="tab3">
+                        <Text>Working on it</Text>
+                      </Tabs.Content>
+                    </Tabs>
                   </Sheet.ScrollView>
                 </Sheet.Frame>
-              </Sheet> */}
+              </Sheet>
             </XStack>
           </XStack>
-
           {/* Center play/pause button */}
-          <XStack justifyContent="center">
-            <Pressable onPress={onPlayPress}>
-              {isPlaying ? <Pause color="white" size={40} /> : <Play color="white" size={40} />}
-            </Pressable>
+          <XStack
+            alignItems="center"
+            gap="$8"
+            // backgroundColor= 'red'
+            position="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)">
+            <RippleButton
+              onPress={() => {
+                if (prevEpisodeIndex >= 0) {
+                  router.push({
+                    pathname: '/watch/[mediaType]',
+                    params: {
+                      mediaType,
+                      provider,
+                      id,
+                      episodeId: episodes[prevEpisodeIndex].id,
+                      episodeDubId: episodes[prevEpisodeIndex].dubId,
+                      isDub: episodes[prevEpisodeIndex].isDub,
+                      poster: episodes[prevEpisodeIndex].image,
+                      title: episodes[prevEpisodeIndex].title,
+                      description: episodes[prevEpisodeIndex].description,
+                      number: episodes[prevEpisodeIndex].number,
+                    },
+                  });
+                }
+              }}>
+              <SkipBack color={prevEpisodeIndex >= 0 ? 'white' : 'gray'} size={30} />
+            </RippleButton>
+            {isBuffering ? (
+              <Spinner size="large" color="white" />
+            ) : (
+              <RippleButton
+                onPress={() => {
+                  onPlayPress();
+                }}>
+                {isPlaying ? <Pause color="white" size={40} /> : <Play color="white" size={40} />}
+              </RippleButton>
+            )}
+            <RippleButton
+              onPress={() => {
+                if (nextEpisodeIndex >= 0) {
+                  router.push({
+                    pathname: '/watch/[mediaType]',
+                    params: {
+                      mediaType,
+                      provider,
+                      id,
+                      episodeId: episodes[nextEpisodeIndex].id,
+                      episodeDubId: episodes[nextEpisodeIndex].dubId,
+                      isDub: episodes[nextEpisodeIndex].isDub,
+                      poster: episodes[nextEpisodeIndex].image,
+                      title: episodes[nextEpisodeIndex].title,
+                      description: episodes[nextEpisodeIndex].description,
+                      number: episodes[nextEpisodeIndex].number,
+                    },
+                  });
+                }
+              }}>
+              <SkipForward color={nextEpisodeIndex >= 0 ? 'white' : 'gray'} size={30} />
+            </RippleButton>
           </XStack>
-
           {/* Bottom Controls */}
           <YStack gap="$2">
             <XStack gap="$2" alignItems="center" justifyContent="space-between" width="100%">
-              <Pressable onPress={onMutePress}>
+              <RippleButton onPress={onMutePress}>
                 {isMuted ? <VolumeOff color="white" size={20} /> : <Volume2 color="white" size={20} />}
-              </Pressable>
+              </RippleButton>
               <XStack gap="$2" marginLeft="auto" alignItems="center">
                 <Button
                   backgroundColor="$color"
@@ -143,12 +289,12 @@ const ControlsOverlay = React.memo(
                   fontSize={13}>
                   +85 s
                 </Button>
-                <Pressable onPress={onFullscreenPress}>
+                <RippleButton onPress={onFullscreenPress}>
                   {isFullscreen ? <Minimize color="white" size={20} /> : <Maximize color="white" size={20} />}
-                </Pressable>
+                </RippleButton>
               </XStack>
             </XStack>
-            <XStack width="100%" alignItems="center" gap="$2" justifyContent="space-between">
+            <XStack width="100%" alignItems="center" gap="$4" justifyContent="space-between">
               <Text color="white" fontSize={13} fontWeight={700}>
                 {formatTime(currentTime)}
               </Text>
@@ -177,5 +323,5 @@ const ControlsOverlay = React.memo(
     ) : null;
   },
 );
-
+ControlsOverlay.displayName = 'ControlsOverlay';
 export default ControlsOverlay;
